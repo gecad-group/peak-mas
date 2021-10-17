2df7dd2a510babe4fcb6bec1890c4820a6dea7af
Bruno Ribeiro <brgri@isep.ipp.pt>
2021-10-17 18:09:22 +0100

    updated ignore

--- examples/exemplo_basico.py
+++
@@ -1,6 +0,0 @@
-import mas
-
-
-agent = mas.Agent('agent1', 'mas.gecad.isep.ipp.pt', 'mas')
-
-mas.run(agent)
--- examples/exemplo_sincronizacao/sincronizador.py
+++
@@ -1,6 +0,0 @@
-import mas 
-
-
-sync = mas.Synchronizer('mas.gecad.isep.ipp.pt', 'mas', 4, 0.1, 5)
-
-mas.run(sync)
