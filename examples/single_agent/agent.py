2114a91edc7bb4e7e2a601a4ce8e0973e61a20c4
brgri <brgri@isep.ipp.pt>
2022-02-03 17:25:07 +0000

    Update agent.py

--- examples/single_agent/agent.py
+++ examples/single_agent/agent.py
@@ -5,10 +5,10 @@ class agent(Agent):
     class HelloWorld(OneShotBehaviour):
 
         async def run(self):
-            msg = Message(to='peak@mas.gecad.isep.ipp.pt')
+            msg = Message(to='peak@conference.mas.gecad.isep.ipp.pt')
             msg.body = 'Hello World'
             await self.send_to_group(msg)
 
     async def setup(self):
-        await self.join_group('peak@mas.gecad.isep.ipp.pt')
+        await self.join_group('peak@conference.mas.gecad.isep.ipp.pt')
         self.add_behaviour(self.HelloWorld())
