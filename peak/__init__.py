d82145039f95afffa3a3d4851a47799d540a1e3b
Bruno Ribeiro <brgri@isep.ipp.pt>
2022-02-18 00:22:09 +0000

    removed unecessary logs

--- peak/__init__.py
+++ peak/__init__.py
@@ -5,7 +5,12 @@ import logging as _logging
 
 _logging.getLogger('aiosasl').setLevel(_logging.CRITICAL)
 _logging.getLogger('spade').setLevel(_logging.CRITICAL)
-_logging.getLogger('agent').setLevel(_logging.CRITICAL)
+_logging.getLogger('muc').setLevel(_logging.CRITICAL)
+_logging.getLogger('StanzaStream').setLevel(_logging.CRITICAL)
+_logging.getLogger('XMLStream').setLevel(_logging.CRITICAL)
+_logging.getLogger('aioopenssl').setLevel(_logging.CRITICAL)
+_logging.getLogger('aioxmpp').setLevel(_logging.CRITICAL)
+_logging.getLogger('StanzaStream').setLevel(_logging.CRITICAL)
 
 __author__ = """Bruno Ribeiro"""
 __email__ = "brgri@isep.ipp.pt"
