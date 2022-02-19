d20f371262fc877338a612e4c2465b931c8c8e0b
Bruno Ribeiro <brgri@isep.ipp.pt>
2022-02-19 18:47:50 +0000

    uncauth execption function didnt work

--- peak/__init__.py
+++ peak/__init__.py
@@ -14,15 +14,6 @@ _logging.getLogger('aioxmpp').setLevel(_logging.ERROR)
 _logging.getLogger('asyncio').setLevel(_logging.ERROR)
 
 
-def handle_exception(exc_type, exc_value, exc_traceback):
-    if issubclass(exc_type, KeyboardInterrupt):
-        sys.__excepthook__(exc_type, exc_value, exc_traceback)
-        return
-
-    _logging.error("Uncaught exception", exc_info=(exc_type, exc_value, exc_traceback))
-
-sys.excepthook = handle_exception
-
 __author__ = """Bruno Ribeiro"""
 __email__ = "brgri@isep.ipp.pt"
 __version__ = "1.0.0"
