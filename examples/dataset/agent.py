67f449204722469eae2589ba42f2e76a5cbb11e4
Bruno Ribeiro <brgri@isep.ipp.pt>
2022-03-19 14:51:10 +0000

    added example with dataset

---
+++ examples/dataset/agent.py
@@ -0,0 +1,24 @@
+
+from peak.mas import Agent, Message
+from peak.mas.behaviour import PeriodicBehaviour
+
+class agent(Agent):
+
+    class PrintMessages(PeriodicBehaviour):
+
+        async def on_start(self):
+            self.count = 0
+
+        async def run(self):
+            msg = Message()
+            msg.to = 'peak@conference.localhost'
+            msg.body = str(self.agent.message[self.count])
+            await self.send_to_group(msg)
+
+            if self.count == 5:
+                await self.agent.stop()
+            self.count += 1
+
+    async def setup(self):
+        await self.join_group('peak@conference.localhost')
+        self.add_behaviour(self.PrintMessages(2))
---
+++ examples/dataset/dataset.py
@@ -0,0 +1,9 @@
+from peak.mas import Properties, Property
+
+class dataset(Properties):
+
+    def build_dataset(self):
+        with open('texts.txt') as f:
+            lines = f.readlines()
+
+        self.add_property(self.agent_name, 'message', Property(lines, loop=True))
