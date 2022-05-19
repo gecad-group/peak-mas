0cbe8f0a5febc5fae4270b9ee7c9ed4c8641dbab
Bruno Ribeiro <brgri@isep.ipp.pt>
2022-05-19 22:25:23 +0100

    added dependecies

--- setup.py
+++ setup.py
@@ -1,4 +1,12 @@
 from setuptools import setup, find_packages
+import os
+
+thelibFolder = os.path.dirname(os.path.realpath(__file__))
+requirementPath = thelibFolder + '/requirements.txt'
+install_requires = [] # Here we'll get: ["gunicorn", "docutils>=0.3", "lxml==0.5a7"]
+if os.path.isfile(requirementPath):
+    with open(requirementPath) as f:
+        install_requires = f.read().splitlines()
 
 setup(
     name="PEAK", 
@@ -7,6 +15,7 @@ setup(
     author_email='brgri@isep.ipp.pt', 
     description='Python-based Ecosystem for Agent Communities', 
     packages=find_packages(),
+    install_requires=install_requires,
     entry_points={
         "console_scripts": [
             "peak=peak.__main__:main"
