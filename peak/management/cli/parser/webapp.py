333eb53050dd56fab33e180fe5c21f497e05d4f4
Bruno Ribeiro <brgri@isep.ipp.pt>
2022-06-22 10:56:55 +0100

    corrected dir when webapp is execution

--- peak/management/cli/parser/webapp.py
+++ peak/management/cli/parser/webapp.py
@@ -1,22 +1,22 @@
 from pynpm import NPMPackage
 from pathlib import Path
 from argparse import ArgumentParser
-import peak
-import os
+from peak import __name__ as peak_name
+from os import sep, chdir
 
 def parse(args = None):
-    parser = ArgumentParser(prog = peak.__name__ + ' management webapp')
+    parser = ArgumentParser(prog = peak_name + ' management webapp')
     parser.add_argument('-p','--port', type=str, default='8080')
     parser.add_argument('-d', '--df', type=str, default='localhost:10000')
 
     ns = parser.parse_args(args)
 
-    webapp_path = 'peak' + os.sep + 'management' + os.sep + 'webapp' + os.sep
+    chdir('peak' + sep + 'management' + sep + 'webapp' + sep)
 
-    env = Path(webapp_path + '.env.local').absolute()
+    env = Path('.env.local').absolute()
     with open(env, 'w') as f:
         f.write("VUE_APP_DF='" + ns.df + "'")
 
-    file = Path( webapp_path + 'package.json').absolute()
+    file = Path('package.json').absolute()
     pkg = NPMPackage(str(file))
     pkg.run_script('serve', '--', '--port', ns.port)
