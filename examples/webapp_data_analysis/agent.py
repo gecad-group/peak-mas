cae0c55ec2a1236b9f351b81b03b2a1e486a27f4
Bruno Ribeiro <brgri@isep.ipp.pt>
2022-07-04 16:19:15 +0100

    added webapp data exportation example

---
+++ examples/webapp_data_analysis/agent.py
@@ -0,0 +1,24 @@
+from peak.mas import Agent, ExportData, PeriodicBehaviour
+from random import random
+
+class agent(Agent):
+
+    class RandomTrial(PeriodicBehaviour):
+        async def on_start(self) -> None:
+            self.count = 0
+
+        async def run(self) -> None:
+            if self.count == 10:
+                await self.agent.stop()
+            self.agent.x = random()*100
+            self.agent.y = random()*100
+            self.agent.z = random()*100
+            self.count += 1
+
+    async def setup(self) -> None:
+        self.x = 0
+        self.y = 0
+        self.z = 0
+        self.add_behaviour(self.RandomTrial(1))
+        self.add_behaviour(ExportData('output.json', ['x', 'y', 'z'], 2, True, 'test'))
+        
