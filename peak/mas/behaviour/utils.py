6154b1c5fac103080bd1d2434cc39db3f6970017
Bruno Ribeiro <brgri@isep.ipp.pt>
2022-07-04 16:18:37 +0100

    added data exportation functionality to peak-mas

--- peak/mas/behaviour/utils.py
+++ peak/mas/behaviour/utils.py
@@ -2,6 +2,7 @@ import logging
 import peak.mas as peak
 from peak.management.df.df import df_name
 from json import dumps as json_dumps
+import logging
 
 
 class CreateNode(peak.OneShotBehaviour):
@@ -68,154 +69,145 @@ class LeaveGroup(peak.OneShotBehaviour):
         await self.agent.leave_group(nodes[-1] + '@' + self.domain)
         await self.agent.leave_group(nodes[-1] + '_down@' + self.domain)
 
-class ExportDataToFile(peak.OneShotBehaviour):
+class ExportData(peak.OneShotBehaviour):
 
-    def __init__(self, file_name: str, properties: list, interval: int = None):
+    def __init__(self, file_name: str, properties: list, interval: int = None, to_graph: bool = False, graph_name: str = '', graph_options: str = ''):
+        super().__init__()
         self.interval = interval
         self.file_name = file_name
         self.properties = properties
-
-    async def run(self) -> None:
-        if isinstance(self.agent, peak.SyncAgent):
-            self.agent.add_behaviour(_ExportDataToFileSync(self.file_name, self.properties))
-        else:
-            self.agent.add_behaviour(_ExportDataToFile(self.interval, self.file_name, self.properties))
-
-class ExportDataToGraph(peak.OneShotBehaviour):
-
-    def __init__(self, graph_name: str, properties: list, interval: int = None, graph_options: str = ''):
-        self.interval = interval
-        self.graph_name = graph_name
-        self.properties = properties
-        self.graph_options = graph_options
-
-    async def run(self) -> None:
-        if isinstance(self.agent, peak.SyncAgent):
-            self.agent.add_behaviour(_ExportDataToDFSync(self.graph_name, self.properties, self.graph_options))
-        else:
-            self.agent.add_behaviour(_ExportDataToDF(self.interval, self.graph_name, self.properties, self.graph_options))
-
-class _ExportDataToFile(peak.PeriodicBehaviour):
-
-    def __init__(self, interval: int, file_name: str, properties: list):
-        super().__init__(interval, None)
-        self.file_name = file_name
-        self. data = {}
-        for property in properties:
-            self.data[property] = []
-
-    async def run(self) -> None:
-        for property in self.data:
-            attribute = getattr(self.agent, property)
-            if type(attribute) is peak.Property:
-                self.data[property].append(attribute.current_value)
-            else:
-                self.data[property].append(attribute)
-
-    async def on_end(self) -> None:
-        with open(self.file_name, 'w') as f:
-            f.write(json_dumps(self.data))
-
-class _ExportDataToDF(peak.PeriodicBehaviour):
-
-    def __init__(self, interval: int, graph_name: str, properties: list, graph_options = ''):
-        super().__init__(interval, None)
+        self.to_graph = to_graph
+        if graph_name == '' and self.to_graph:
+            raise Exception('if graph is set to true, it must have a name')
         self.graph_name = graph_name
         self.graph_options = graph_options
-        self. data = {}
-        for property in properties:
-            self.data[property] = []
-
-    async def on_start(self) -> None:
-        msg = peak.Message(to=df_name(self.agent.jid.domain))
-        msg.body = json_dumps(self.data)
-        msg.metadata = {
-            'graph_name': self.graph_name,
-            'graph_options': self.graph_options,
-            'resource': 'dataanalysis'
-        }
-        self.send(msg)
 
     async def run(self) -> None:
-        for property in self.data:
-            attribute = getattr(self.agent, property)
-            if type(attribute) is peak.Property:
-                self.data[property].append(attribute.current_value)
-            else:
-                self.data[property].append(attribute)
-        msg = peak.Message(to=df_name(self.agent.jid.domain))
-        msg.body = json_dumps(self.data)
-        msg.metadata = {
-            'graph_name': self.graph_name,
-            'graph_options': self.graph_options,
-            'resource': 'dataanalysis'
-        }
-        self.send(msg)
+        logger = logging.getLogger(self.__class__.__name__)
+        if isinstance(self.agent, peak.SyncAgent):
+            logger.debug('synchronizer detected')
+            self.agent.add_behaviour(_ExportDataSync(self.file_name, self.properties, self.to_graph, self.graph_name, self.graph_options))
+        else:
+            logger.debug('synchronizer not detected')
+            self.agent.add_behaviour(_ExportData(self.file_name, self.properties, self.interval, self.to_graph, self.graph_name, self.graph_options))
 
-class _ExportDataToFileSync(peak.CyclicBehaviour):
+class _ExportDataSync(peak.CyclicBehaviour):
 
-    def __init__(self, file_name: str, properties: list):
+    def __init__(self, file_name: str, properties: list, to_graph: bool, graph_name: str, graph_options: str):
         super().__init__()
         self.file_name = file_name
-        self. data = {}
-        for property in properties:
-            self.data[property] = []
+        self.graph_name = graph_name
+        self.properties = properties
+        self.to_graph = to_graph
+        self.graph_options = graph_options
 
+    async def on_start(self) -> None:
+        self.logger = logging.getLogger(self.__class__.__name__)
+        self.logger.info('behaviour initiated')
+        template_step = peak.Template()
+        template_step.set_metadata('sync', 'step')
+        template_stop = peak.Template()
+        template_stop.set_metadata('sync', 'stop')
+        template = template_step | template_stop
+        self.set_template(template)
+
+        self.file_data = {}
+        for property in self.properties:
+            self.file_data[property] = []
+        if self.to_graph:
+            msg = peak.Message(to=df_name(self.agent.jid.domain))
+            msg.body = 'Create graph ' + self.graph_name
+            msg.metadata = {
+                'resource': 'graph',
+                'action': 'create',
+                'graph_name': self.graph_name,
+                'graph_options': self.graph_options,
+                'properties': json_dumps(self.properties)
+            }
+            await self.send(msg)
+            self.logger.info('creating graph '+  + self.graph_name)
+    
     async def run(self) -> None:
         msg = await self.receive(60)
         if msg:
             if msg.get_metadata('sync') == 'step':
-                for property in self.data:
+                current_data = dict()
+                for property in self.file_data:
                     attribute = getattr(self.agent, property)
                     if type(attribute) is peak.Property:
-                        self.data[property].append(attribute.current_value)
+                        self.file_data[property].append(attribute.current_value)
+                        current_data[property] = attribute.current_value
                     else:
-                        self.data[property].append(attribute)
+                        self.file_data[property].append(attribute)
+                        current_data[property] = attribute
+                if self.to_graph:
+                    msg = peak.Message(to=df_name(self.agent.jid.domain))
+                    msg.body = 'Update graph ' + self.file_name
+                    msg.metadata = {
+                        'resource': 'graph',
+                        'action': 'update',
+                        'graph_name': self.file_name,
+                        'data': json_dumps(current_data)
+                    }
+                    await self.send(msg)
+                    self.logger.info('updating graph '+  + self.graph_name)
             if msg.get_metadata('sync') == 'stop':
                 with open(self.file_name, 'w') as f:
                     f.write(json_dumps(self.data))
-        
-class _ExportDataToDFSync(peak.CyclicBehaviour):
 
-    def __init__(self, graph_name: str, properties: list, graph_options = ''):
-        super().__init__()
+class _ExportData(peak.PeriodicBehaviour):
+
+    def __init__(self, file_name: str, properties: list, interval: float, to_graph: bool, graph_name: str, graph_options: str):
+        super().__init__(interval, None)
+        self.file_name = file_name
         self.graph_name = graph_name
+        self.properties = properties
+        self.to_graph = to_graph
         self.graph_options = graph_options
-        self. data = {}
-        for property in properties:
-            self.data[property] = []
 
     async def on_start(self) -> None:
-        msg = peak.Message(to=df_name(self.agent.jid.domain))
-        msg.body = json_dumps(self.data)
-        msg.metadata = {
-            'graph_name': self.graph_name,
-            'graph_options': self.graph_options,
-            'resource': 'dataanalysis'
-        }
-        self.send(msg)
-
-    async def run(self) -> None:
-        msg = await self.receive(60)
-        if msg and msg.get_metadata('sync') == 'step':
-            for property in self.data:
-                attribute = getattr(self.agent, property)
-                if type(attribute) is peak.Property:
-                    self.data[property].append(attribute.current_value)
-                else:
-                    self.data[property].append(attribute)
+        self.logger = logging.getLogger(self.__class__.__name__)
+        self.logger.info('exporting '+ str(self.properties))
+        self.file_data = {}
+        for property in self.properties:
+            self.file_data[property] = []
+        if self.to_graph:
             msg = peak.Message(to=df_name(self.agent.jid.domain))
-            msg.body = json_dumps(self.data)
+            msg.body = 'Create graph ' + self.graph_name
             msg.metadata = {
+                'resource': 'graph',
+                'action': 'create',
                 'graph_name': self.graph_name,
                 'graph_options': self.graph_options,
-                'resource': 'dataanalysis'
+                'properties': json_dumps(self.properties)
             }
-            self.send(msg)
-   
-
+            await self.send(msg)
+            self.logger.info('creating graph ' + self.graph_name)
+        
+    async def run(self) -> None:
+        current_data = dict()
+        for property in self.file_data:
+            attribute = getattr(self.agent, property)
+            if type(attribute) is peak.Property:
+                self.file_data[property].append(attribute.current_value)
+                current_data[property] = attribute.current_value
+            else:
+                self.file_data[property].append(attribute)
+                current_data[property] = attribute
+            self.logger.debug('exporting property "' + property+ '": ' + str(current_data[property]))
+        if self.to_graph:
+            msg = peak.Message(to=df_name(self.agent.jid.domain))
+            msg.body = 'Update graph ' + self.graph_name
+            msg.metadata = {
+                'resource': 'graph',
+                'action': 'update',
+                'graph_name': self.graph_name,
+                'data': json_dumps(current_data)
+            }
+            await self.send(msg)
+            self.logger.debug('updating graph ' + self.graph_name)
     
-
-
-
-
+    async def on_end(self) -> None:
+        self.logger.info('exporting to file "' + self.file_name + '"')
+        with open(self.file_name, 'w') as f:
+            f.write(json_dumps(self.file_data))
