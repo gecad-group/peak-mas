e82844d4b3401594cafb587b2f8f6be191707c41
Bruno Ribeiro <bruno.r.ribeiro@outlook.pt>
2022-09-13 15:37:23 +0100

    doc: add agent execution example

---
+++ examples/agent_execution/agent.py
@@ -0,0 +1,11 @@
+from peak import Agent
+from peak.behaviours import OneShotBehaviour
+
+class agent(Agent):
+    class HelloWorld(OneShotBehaviour):
+        async def run(self) -> None:
+            print('Hello World')
+            await self.agent.stop()
+
+    async def setup(self) -> None:
+        self.add_behaviour(self.HelloWorld())
