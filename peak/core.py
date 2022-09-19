a1843b36ff2c620fd2ce7d267ba7aa1ed2446221
Bruno Ribeiro <bruno.r.ribeiro@outlook.pt>
2022-09-19 16:08:05 +0100

    doc: document core.py

--- peak/core.py
+++ peak/core.py
@@ -1,7 +1,7 @@
 import asyncio as _asyncio
 import logging as _logging
 from abc import ABCMeta as _ABCMeta
-from typing import List
+from typing import Any, List
 
 import aioxmpp as _aioxmpp
 import spade as _spade
@@ -11,16 +11,14 @@ _logger = _logging.getLogger(__name__)
 
 
 class _XMPPAgent(_spade.agent.Agent):
-    """This class integrates the XMPP Multi-User Chat (MUC) feature into the SPADE arquitecture."""
+    """Agent that integrates XMPP functionalities.
+    
+    Attributes:
+        jid: XMPP identifier.
+        verify_security: If true verifies the SSL certificates.
+    """
 
-    def __init__(self, jid: JID, verify_security=False):
-        """Agent base class.
-
-        Args:
-            name (str): Name of the agent.
-            server (str): Domain of the XMPP server to connect the agent to.
-            verify_security (bool, optional): Wether to verify or not the SSL certificates. Defaults to False.
-        """
+    def __init__(self, jid: JID, verify_security: bool=False):
         _logging.getLogger(jid.localpart).setLevel(_logging.ERROR)
         self.groups = dict()
         self.muc_client = None
@@ -29,8 +27,7 @@ class _XMPPAgent(_spade.agent.Agent):
         super().__init__(jid, pw, verify_security)
 
     async def _hook_plugin_after_connection(self):
-        """
-        Executed after SPADE Agent's connection.
+        """Executed after SPADE Agent's connection.
 
         This method adds the MUC service to the Agent XMPP Client and
         adds a message dispatcher for the group(MUC) messages.
@@ -52,14 +49,21 @@ class _XMPPAgent(_spade.agent.Agent):
         )
 
     def _on_muc_failure_handler(self, exc):
-        """
-        Handles MUC failed connections.
+        """Handles MUC failed connections.
         """
 
         _logger.critical("Failed to enter MUC room")
         raise exc
 
-    async def join_group(self, jid):
+    async def join_group(self, jid: str):
+        """Joins the agent in a group.
+
+        Args:
+            jid: Group's XMPP identifier.
+
+        Raises:
+            Exception if the group JID is invalid.
+        """
         room, fut = self.muc_client.join(_aioxmpp.JID.fromstr(jid), self.name)
         room.on_failure.connect(self._on_muc_failure_handler)
         await _asyncio.wait([fut], timeout=3)
@@ -70,26 +74,39 @@ class _XMPPAgent(_spade.agent.Agent):
         else:
             raise Exception("invalid group JID: " + str(jid))
 
-    async def leave_group(self, jid):
+    async def leave_group(self, jid: str):
+        """Leaves a group.
+
+        Args:
+            jid: Group's XMPP identifier.
+        """
         room = self.groups.pop(jid, None)
         if room:
             _logger.info("leaving group: " + jid)
             await room.leave()
 
-    async def list_groups(self, node_jid):
+    async def list_groups(self, node_jid: str):
+        """Retrieves the list of the existing groups.
+
+        Args:
+            node_jid: JID of the domain responsable for the MUC functionality.
+
+        Returns:
+            A list of XMPP groups.
+        """
         info = await self.disco.query_items(
             _aioxmpp.JID.fromstr(node_jid), require_fresh=True
         )
         return info.items
 
-    async def group_members(self, jid) -> List:
-        """Extracts list of group members from a group chat.
+    async def group_members(self, jid: str) -> List:
+        """Extracts list of group members from a group.
 
         Args:
-            group (str): Name of the group. Must be registered previously in the gorup
+            jid: Group's XMPP identifier.
 
         Returns:
-            List: A copy of the list of occupants. The local user is always the first item in the list.
+            A copy of the list of occupants. The local user is always the first item in the list.
         """
         if jid in self.groups:
             return self.groups[jid].members
@@ -101,41 +118,45 @@ class _XMPPAgent(_spade.agent.Agent):
 
 
 class Agent(_XMPPAgent):
-    def __init__(self, jid: JID, properties=None, verify_security=False):
+    """PEAK's base agent.
+    
+    Attributes:
+        jid: XMPP identifier.
+        properties: Properties to be injected in the agent.
+        verify_security: If True, it verifies the SSL certificates.
+    """
+    def __init__(self, jid: JID, properties: Any=None, verify_security: bool=False):
+        """Inits Agent and fills it with properties."""
         super().__init__(jid, verify_security=verify_security)
         if properties:
             self.properties = properties
-            self._parse(properties)
+            for key in properties:
+                setattr(self, key, properties[key])
 
     def iterate_properties(self):
+        """Iterates one index over the properties.
+        """
         if hasattr(self, "properties"):
             for key in self.properties:
                 attr = getattr(self, key)
                 if attr:
                     getattr(self, key).next()
-
-    def _parse(self, properties):
-        for key in properties:
-            setattr(self, key, properties[key])
-
+      
 
 class _Behaviour:
+    """Adds functinality to the SPADE base behaviours.
+    """
 
     agent: Agent
 
     async def send_to_group(self, msg: _spade.message.Message):
-        """Sends a message to a group chat.
+        """Sends a message to a group.
 
-        When sending a message to a group the agent joins the group first. The parameter
-        'leave' tells the method if the agent leaves, or not, the group after sending the
-        message. (If the intention is to send a single request to a new group the best
-        option would be to leave the group chat, if the intention is to send a message
-        to a group wich the agent already belongs to, it's better to not leave)
+        If the agent is not in the group, the agent enters the room first, 
+        sends the message and then leaves the group.
+
         Args:
-            msg (mas.Message): The Message.
-            group (str, optional): Name of the group to send the message to. If None is given the
-                                   the message is sent to the MAS group. Defaults to None.
-            leave (bool, optional): If true, agent leaves the group after sending the message. Defaults to False.
+            msg: The XMPP message.
         """
         raw_msg = msg.prepare()
         try:
