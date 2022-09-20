bfd36f41fc809fd6559d33fdd2d6883c511b3d44
Bruno Ribeiro <bruno.r.ribeiro@outlook.pt>
2022-09-20 16:45:58 +0100

    refactor: elaborate arg parser function

--- peak/__main__.py
+++ peak/__main__.py
@@ -1,25 +1,42 @@
-from logging import getLogger
+import logging
 from sys import argv
 
 from peak.cli import df, mas
 from peak import __name__ as peak_name
 
+from argparse import ArgumentParser
+
 
 def main(args=None):
-    if not args:
-        args = argv[1:]
-    if len(args) == 0 or args[0].lower() == "-h":
-        print("Help message - in development")
-    elif args[0].lower() == "df":
-        df.exec(args[1:])
-    elif args[0].lower() == "run":
+    parser = ArgumentParser(prog=peak_name)
+    parser.add_argument('-version', action='version', version='PEAK 1.0')
+    subparsers = parser.add_subparsers(required=True)
+
+    # parser for DF agent
+    df_parser = subparsers.add_parser(name='df', help='Executes Directory Facilitator agent.')
+    df_parser.add_argument("-domain", type=str, default="localhost", help='XMPP domain to which the DF must register and login.')
+    df_parser.add_argument("--verify_security", action='store_true', help='Verifies the SLL certificates.')
+    df_parser.add_argument(
+        "-log",
+        type=lambda x: logging.getLevelName(str.upper(x)),
+        default=logging.getLevelName("INFO"),
+        help='Selects the logging level of the DF.'
+    )
+    df_parser.add_argument("-port", type=str, default="10000", help='Port to be opened for the REST API.')
+    df_parser.set_defaults(func=df.exec)
+
+    # parser for the agent execution
+    run_parser = subparsers.add_parser(name='run', help='Executes PEAK agents.')
+    args = parser.parse_args(args)
+
+    if args[0].lower() == "run":
         mas.exec(args[1:])
     else:
         print("Help message - in development")
 
 
 if __name__ == "__main__":
-    logger = getLogger(peak_name)
+    logger = logging.getLogger(peak_name)
     try:
         main()
     except Exception as e:
--- peak/cli/df.py
+++ peak/cli/df.py
@@ -12,18 +12,6 @@ def exec(args: list[str]):
     Args:
         args: List of arguments to be used by the DF.
     """
-    parser = ArgumentParser(prog=peak.__name__ + "df")
-    parser.add_argument("-d", "--domain", type=str, default="localhost")
-    parser.add_argument("--verify_security", type=bool, default=False)
-    parser.add_argument(
-        "-l",
-        "--log",
-        type=lambda x: logging.getLevelName(str.upper(x)),
-        default=logging.getLevelName("INFO"),
-    )
-    parser.add_argument("-p", "--port", type=str, default="10000")
-    ns = parser.parse_args(args)
-
     logging.basicConfig(
         level=ns.log,
         format="%(asctime)s [%(levelname)s] %(message)s",
