b14a509e37b9dbfd350873915132c9a9e4820806
Bruno Ribeiro <bruno.r.ribeiro@outlook.pt>
2022-09-20 11:13:32 +0100

    refactor: migrate paser function to __main__.py

--- peak/__main__.py
+++ peak/__main__.py
@@ -1,14 +1,21 @@
 from logging import getLogger
 from sys import argv
 
-import peak.cli.parser as parser
+from peak.cli import df, mas
 from peak import __name__ as peak_name
 
 
 def main(args=None):
     if not args:
         args = argv[1:]
-    parser.parse(args)
+    if len(args) == 0 or args[0].lower() == "-h":
+        print("Help message - in development")
+    elif args[0].lower() == "df":
+        df.exec(args[1:])
+    elif args[0].lower() == "run":
+        mas.exec(args[1:])
+    else:
+        print("Help message - in development")
 
 
 if __name__ == "__main__":
