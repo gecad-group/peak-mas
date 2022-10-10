5888415eaf076d7f506dfafc6d92531169071ce1
Bruno Ribeiro <bruno.r.ribeiro@outlook.pt>
2022-10-10 11:29:10 +0100

    doc: fix multi-user chat/single group example

--- examples/multi_user_chat/single_group/agent.py
+++ examples/multi_user_chat/single_group/agent.py
@@ -1,20 +1,19 @@
-from peak import Agent, JoinGroup
-
+from peak import Agent, JoinGroup, OneShotBehaviour, Message
+from asyncio import sleep
 
 class agent(Agent):
+
+    class HelloWorld(OneShotBehaviour):
+        async def run(self) -> None:
+            await self.execute(JoinGroup("group1", f"conference.{self.agent.jid.domain}"))
+            msg = Message(to=f"group1@conference.{self.agent.jid.domain}")
+            msg.body = "Hello World"
+            await self.send_to_group(msg)
+            await sleep(5)
+            msg.body = "Goodbye World"
+            await self.send_to_group(msg)
+            await self.agent.stop()
+
     async def setup(self) -> None:
-        self.add_behaviour(
-            JoinGroup("group1", "conference.localhost", ["test", "awesome", "cool"])
-        )
-        self.add_behaviour(
-            JoinGroup("group2", "conference.localhost", ["test", "awesome"])
-        )
-        self.add_behaviour(
-            JoinGroup(
-                "group3",
-                "conference.localhost",
-                [
-                    "test",
-                ],
-            )
-        )
+        self.add_behaviour(self.HelloWorld())
+
