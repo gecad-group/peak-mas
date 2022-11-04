b14687546f8e01e8e28d48aa3f0bcffd347734e0
Bruno Ribeiro <bruno.r.ribeiro@outlook.pt>
2022-11-04 10:00:53 +0000

    refactor: move peak folder to src folder

--- src/peak/__init__.py
+++ src/peak/__init__.py
@@ -17,8 +17,9 @@ from peak.agents import *
 from peak.behaviours import *
 from peak.properties import *
 
-__author__ = """Bruno Ribeiro"""
+__author__ = "Bruno Ribeiro"
 __email__ = "brgri@isep.ipp.pt"
+__version__ = "1.0.0"
 
 # filtering noisy loggers
 _logging.getLogger("aiosasl").setLevel(_logging.ERROR)
