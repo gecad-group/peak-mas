1bd57717928e2c5392fd72d530af0b8c0e09b324
Bruno Ribeiro <bruno.r.ribeiro@outlook.pt>
2022-11-16 14:14:25 +0000

    chore: fix dataset example

--- examples/data_providers/dataset/dataset.py
+++ examples/data_providers/dataset/dataset.py
@@ -4,6 +4,6 @@ from peak import Properties, Property
 class dataset(Properties):
     def build_dataset(self):
         with open("texts.txt") as f:
-            lines = f.readlines()
+            lines = f.read().splitlines()
 
-        self.add_property(self.agent_name, "message", Property(lines, loop=True))
+        self.add_property("message", Property(lines, loop=True))
