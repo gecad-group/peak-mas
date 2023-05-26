646639b4d0e36cd48b9277e62ccdde513f9bd852
Bruno Ribeiro <bruno.r.ribeiro@outlook.pt>
2023-05-26 17:22:11 +0100

    docs: add logging example

--- examples/2_multiagent_system/receiver.py
+++ examples/2_multiagent_system/receiver.py
@@ -1,11 +1,17 @@
-from peak import Agent, OneShotBehaviour
+from peak import Agent, OneShotBehaviour, getLogger, log
+
+logger = getLogger(__name__)
 
 
 class receiver(Agent):
     class ReceiveHelloWorld(OneShotBehaviour):
         async def run(self):
+            logger.info("waiting for message")
             msg = await self.receive()
+            logger.info("message received")
             print(f"{msg.sender} sent me a message: '{msg.body}'")
+            logger.info("agent stop")
+            log("testing testing")
             await self.agent.stop()
 
     async def setup(self):
