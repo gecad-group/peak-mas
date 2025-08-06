ffc3840bf2dec5301f0dd86f209dcdc04892b757
Bruno Ribeiro <brgri@isep.ipp.pt>
2025-08-06 16:42:09 +0100

    fix: message not supporting JID as an input for to and sender attributes

--- src/peak/message.py
+++ src/peak/message.py
@@ -1,15 +1,72 @@
 import aioxmpp
 import aioxmpp.forms.xso as forms_xso
-from aioxmpp import MessageType
+from aioxmpp import MessageType, JID
 from spade.message import SPADE_X_METADATA
-from spade.message import Message as _SpadeMessage
+from spade.message import Message as _Message, MessageBase as _MessageBase
+from typing import Union
 
 from peak.logging import getLogger
 
 logger = getLogger(__name__)
 
+class MessageBase(_MessageBase):
+    @property
+    def to(self) -> aioxmpp.JID:
+        """
+        Gets the jid of the receiver.
 
-class Message(_SpadeMessage):
+        Returns:
+          aioxmpp.JID: jid of the receiver
+
+        """
+        return self._to
+
+    @to.setter
+    def to(self, jid: Union[str, JID]) -> None:
+        """
+        Set jid of the receiver.
+
+        Args:
+          jid (str): the jid of the receiver.
+
+        """
+        if isinstance(jid, JID):
+            self._to = jid
+            return
+        if isinstance(jid, str):
+            self._to = JID.fromstr(jid)
+        if jid is not None and not isinstance(jid, str) and not isinstance(jid, JID):
+            raise TypeError("'to' MUST be a string or a JID")
+        
+    @property
+    def sender(self) -> aioxmpp.JID:
+        """
+        Get jid of the sender
+
+        Returns:
+          aioxmpp.JID: jid of the sender
+
+        """
+        return self._sender
+        
+    @sender.setter
+    def sender(self, jid: Union[str, JID]) -> None:
+        """
+        Set jid of the sender
+
+        Args:
+          jid (str): jid of the sender
+
+        """
+        if isinstance(jid, JID):
+            self._sender = jid
+            return
+        if isinstance(jid, str):
+            self._sender = JID.fromstr(jid)
+        if jid is not None and not isinstance(jid, str) and not isinstance(jid, JID):
+            raise TypeError("'sender' MUST be a string or a JID")
+
+class Message(MessageBase, _Message):
     def prepare(self) -> aioxmpp.Message:
         """
         Returns an aioxmpp.stanza.Message built from the Message and prepared to be sent.
